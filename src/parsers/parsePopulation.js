09b08593d0c2732ced90cd8e6585d01fe78dc091
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-04-14 18:58:54 +0100

    Update and rename parsePopulations.js to parsePopulation.js

---
+++ src/parsers/parsePopulation.js
@@ -0,0 +1,73 @@
+import filterTypes from "../filters/filtersTypes.js";
+import { DataTypes0, DataTypes1 } from "../filters/filterDataTypes.js";
+
+function parsePopulation(encoded_populations) {
+  const buckets = {};
+  const filters = [];
+
+  // find buckets
+  population[0].forEach((bucket) => {
+    buckets[bucket[0].toString()] = {
+      rollout: [],
+    };
+
+    bucket[1].forEach((rollout) => {
+      buckets[bucket[0].toString()].rollout.push({
+        start: rollout.s,
+        end: rollout.e,
+      });
+    });
+  });
+
+  // find filters:
+  population[1].forEach((filter) => {
+    if (Object.keys(filterTypes).includes(filter[0].toString())) {
+      for (let data of filter[1]) {
+        if (filterTypes[filter[0]]?.requiredData?.includes(data[0])) {
+          let filter_ = {};
+
+          filter_.type = filterTypes[filter[0]].type;
+          filter_[DataTypes0[data[0]]] = data[1];
+          filters.push(filter_);
+        } else {
+          console.log(
+            `[ERROR] Excepted key to be in required data list but got ${
+              DataTypes0[data[0]]
+            }`,
+            filter
+          );
+        }
+      }
+    } else {
+      filters.push({
+        type: filterTypes.default.type,
+      });
+    }
+  });
+  // merge filters with same type
+  const mergedFilters = [];
+  const repeatedTypes = {};
+  for (let filter of filters) {
+    if (!repeatedTypes[filter.type]) repeatedTypes[filter.type] = [];
+    repeatedTypes[filter.type].push(filter);
+  }
+  for (let repeatedType in repeatedTypes) {
+    const data = {};
+    for (let filter of repeatedTypes[repeatedType]) {
+      for (let key in filter) {
+        if (key === "type") continue;
+        data[key] = filter[key];
+      }
+    }
+    mergedFilters.push({ type: repeatedType, ...data });
+  }
+
+
+
+  return {
+    buckets: buckets,
+    filters: mergedFilters,
+  });
+}
+
+export { parsePopulation };
