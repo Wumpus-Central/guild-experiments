554767196b2c74214ac17f7b4affaa4eb002533b
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-04-14 18:59:16 +0100

    Update decoder.js

--- src/decoder.js
+++ src/decoder.js
@@ -15,7 +15,7 @@ function decodeExperiment(experiment){
     decodedExperiment.hash = experiment[0]
     decodedExperiment.revision = experiment[2]
     decodedExperiment.id = experiment[1] ?? null
-    decodedExperiment.populations = parsePopulations(experiment[3])
+    decodedExperiment.populations = experiment[3].map(parsePopulation)
     // discord fucking has too many arrays on these that's why we flat them before running the loop:
     decodedExperiment.overridesFormatted = parsePopulations(experiment?.[5]?.flat() || [])    
 
